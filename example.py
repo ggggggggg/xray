e8b112241eaabe589108e583408d1497bbd79f62
Galen O'Neil <galen.oneil@nist.gov>
2014-02-14 17:39:19 -0700

    added optics and source data

--- example.py
+++ example.py
@@ -2,29 +2,22 @@ import xray, numpy, pylab
 
 # put materials and thickness in microns in m dictionary
 m={}
-m['Air']=1e5
-m['Kapton']=25*4
-m['Al']=10
+m['Air']=2e5
+m['Kapton']=25*8
+m['Al']=20
+m['Water']=120
 
-N0 = 1
-T = 2000 #K
-
+e_pulse = {"XOSpolycapillary_1":3.2,"XOSpolycapillary_2":4.125}
 
 pylab.figure()
 for optic_name, optic in xray.optics.iteritems():
     energies = numpy.linspace(optic.energies[0], optic.energies[-1])
-    flux = xray.source_optic_materials(N0,T, optic_name, m, energies=energies)
-    pylab.plot(energies,flux,label=optic_name)
+    flux = xray.source_optic_materials_absorber(e_pulse[optic_name], optic_name, m, energies=energies)
+    pylab.plot(energies,flux,label="%s, %d mJ"%(optic_name, e_pulse[optic_name]))
 pylab.xlabel('energy (eV)')
 pylab.ylabel('flux (arb)')
 pylab.legend()
 pylab.xlim(3000,25000)
-
-
-#pylab.figure()
-#for name, material in xray.materials.iteritems():
-#    pylab.plot(material.energies, material.transmission(material.energies), label=material.name)
-#pylab.legend()
-pylab.figure()
-energies=numpy.linspace(1000,30000,100)
-pylab.plot(energies, xray.materials_transmission(energies, m))
+pylab.title(repr(m))
+pylab.minorticks_on()
+pylab.grid()
--- xray.py
+++ xray.py
@@ -3,7 +3,7 @@ from scipy.interpolate import UnivariateSpline
 
 datapath=os.path.join(os.path.dirname(os.path.abspath(__file__)),"transmission_data")
 
-class Transmission():
+class Material():
     def __init__(self, name, energies, absorption_lengths_um, density=1.0):
         self.name=name
         self.energies=numpy.array(energies)
@@ -14,6 +14,9 @@ class Transmission():
         if density is None: density = self.density
         length_um = length_um*density/float(self.density)
         return numpy.exp(-length_um/numpy.interp(energies, self.energies, self.absorption_lengths_um))
+    
+    def absorbtion(self, energies, length_um=1, density=None):
+        return 1-self.transmission(energies, length_um, density)
         
 class Optic():
     def __init__(self, name, energies, transmission_data, solid_angle=1):
@@ -26,17 +29,35 @@ class Optic():
         if solid_angle is None: solid_angle=self.solid_angle
         transmission_data = self.transmission_data*solid_angle/self.solid_angle
         return numpy.interp(energies, self.energies, transmission_data)
-        
-def read_transmission_files():
+    
+class PlasmaSource():
+    def __init__(self, name, e_pulse, N0, T):
+        self.name=name
+        self.e_pulse=e_pulse
+        self.N0=N0
+        self.T=T
+    
+    def spectrum(self, energies, e_pulse):
+        N0 = numpy.interp(e_pulse, self.e_pulse, self.N0)
+        T =  numpy.interp(e_pulse, self.e_pulse, self.T)
+        return N0*numpy.exp(-numpy.array(energies)/T)
+    
+#    def spectrum(self, energies, e_pulse):
+#        N0 = numpy.interp(e_pulse, self.e_pulse, self.N0)
+#        T =  numpy.interp(e_pulse, self.e_pulse, self.T)
+#        T2=T*0.5
+#        return numpy.exp(6000/T-6000/T2)*N0*numpy.exp(-numpy.array(energies)/T)*(energies>6000)+N0*numpy.exp(-numpy.array(energies)/T2)*(energies<6000)
+            
+def read_material_files():
     datapath=os.path.join(os.path.dirname(os.path.abspath(__file__)),"transmission_data")
     dirList=os.listdir(datapath)
-    transmission_dict={}
+    material_dict={}
     for fname in dirList:
         if '.txt' not in fname:continue
         data = numpy.loadtxt(os.path.join(datapath,fname))
         name=fname[0:-4]
-        transmission_dict[name]=Transmission(name, data[:,0], data[:,1])
-    return transmission_dict
+        material_dict[name]=Material(name, data[:,0], data[:,1])
+    return material_dict
 
 def read_optic_files():
     datapath=os.path.join(os.path.dirname(os.path.abspath(__file__)),"optic_data")
@@ -48,6 +69,18 @@ def read_optic_files():
         name=fname[0:-4]
         optic_dict[name]=Optic(name, data[:,0], data[:,1])
     return optic_dict
+
+def read_source_files():
+    datapath=os.path.join(os.path.dirname(os.path.abspath(__file__)),"source_data")
+    dirList=os.listdir(datapath)
+    source_dict={}
+    for fname in dirList:
+        if '.txt' not in fname:continue
+        data = numpy.loadtxt(os.path.join(datapath,fname))
+        name=fname[0:-4]
+        source_dict[name]=PlasmaSource(name, data[:,0], data[:,1], data[:,2])
+    return source_dict    
+
 
 def plasma_source_spectrum(energies, N0, T):
     """ energy, array of energies in eV 
@@ -61,12 +94,16 @@ def materials_transmission(energies, materials_thickness):
         flux*=materials[material].transmission(energies, thickness)
     return flux
 
-def source_optic_materials(N0,T, optic_name, materials_thickness, solid_angle=None, energies = None):
+def source_optic_materials(e_pulse, optic_name, materials_thickness, solid_angle=None, energies = None, source_name = "water_jet_2013"):
     optic = optics[optic_name]
     if energies is None: energies = optic.energies
-    return plasma_source_spectrum(energies, N0, T)*optic.transmission(energies, solid_angle)*materials_transmission(energies, materials_thickness)
+    return sources[source_name].spectrum(energies, e_pulse)*optic.transmission(energies, solid_angle)*materials_transmission(energies, materials_thickness)
 
-materials=read_transmission_files()
+def source_optic_materials_absorber(e_pulse, optic_name, materials_thickness, solid_angle=None, energies = None, absorber_material='Bi', absober_length_um=2, source_name = "water_jet_2013"):
+    return materials[absorber_material].absorbtion(energies, absober_length_um)*source_optic_materials(e_pulse, optic_name, materials_thickness, solid_angle, energies, source_name)
+    
+materials=read_material_files()
 optics=read_optic_files()
+sources=read_source_files()
         
             
